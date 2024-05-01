f3b59cdfbd33981796e2fee8a8efcb684ce1a824
Azeez Abdulsalam <azeezade04@gmail.com>
2024-05-01 10:06:45 +0100

    fixes on update todo dto

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -26,49 +26,27 @@ export class TodosService {
   }
 
   async findOne(id: string) {
-    const task = await this.todoModel.findOne({ _id: id });
+    const todo = await this.todoModel.findOne({ _id: id });
 
-    if (!task) throw new NotFoundException('Task Not Found!');
-    return task;
+    if (!todo) throw new NotFoundException('Todo Not Found!');
+    return todo;
   }
 
   async update(id: string, updateTodoInput: UpdateTodoInput) {
-    return await this.todoModel.findOneAndUpdate(
+    const todo = await this.todoModel.findOneAndUpdate(
       { _id: id },
       { ...updateTodoInput },
       { new: true },
     );
+    if (!todo) throw new NotFoundException('Todo Not Found!');
+
+    return todo;
   }
 
   async remove(id: string) {
-    const deletedTask = await this.todoModel.findByIdAndDelete({ _id: id });
+    const deletedTodo = await this.todoModel.findByIdAndDelete({ _id: id });
 
-    if (!deletedTask) throw new NotFoundException('Task Not Found!');
-    return deletedTask;
+    if (!deletedTodo) throw new NotFoundException('Todo Not Found!');
+    return deletedTodo;
   }
 }
-
-// @Injectable()
-// export class CatsService {
-//   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
-
-//   async create(createCatDto: CreateCatDto): Promise<Cat> {
-//     const createdCat = await this.catModel.create(createCatDto);
-//     return createdCat;
-//   }
-
-//   async findAll(): Promise<Cat[]> {
-//     return this.catModel.find().exec();
-//   }
-
-//   async findOne(id: string): Promise<Cat> {
-//     return this.catModel.findOne({ _id: id }).exec();
-//   }
-
-//   async delete(id: string) {
-//     const deletedCat = await this.catModel
-//       .findByIdAndRemove({ _id: id })
-//       .exec();
-//     return deletedCat;
-//   }
-// }
